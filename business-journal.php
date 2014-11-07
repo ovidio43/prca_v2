7156a0bd8141ed293813be2cb159008c9caaeea6
Aldo Adachi <aldo.adachi@iconicweb.com>
2014-11-07 10:06:17 -0400

    change file and addd journal

---
+++ business-journal.php
@@ -0,0 +1,291 @@
+<?php include("header.php");?>
+  <div class="container">
+    <div class="row">
+      <div class="main-sidebar">
+<div class="accordion" id="leftMenu">
+                    <div class="accordion-group">
+                        <div class="accordion-heading">
+                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#">
+                                  <i class="icon-home"></i> Dashboard
+                            </a>
+                        </div>
+                    </div>
+                    <div class="accordion-group">
+                        <div class="accordion-heading">
+                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#collapseTwo">
+                                <i class="icon-th"></i> Layout
+                            </a>
+                        </div>
+                        <div id="collapseTwo" class="accordion-body collapse" style="height: 0px; ">
+                            <div class="accordion-inner">
+                                <ul>
+                                    <li><a href="">This is one</a></li>
+                                    <li><a href="">This is one</a></li>
+                                    <li><a href="">This is one</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-group">
+                        <div class="accordion-heading">
+                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#collapseThree">
+                                <i class="icon-th-list"></i> UI Components
+                            </a>
+                        </div>
+                        <div id="collapseThree" class="accordion-body collapse" style="height: 0px; ">
+                            <div class="accordion-inner">
+                                <ul>
+                                    <li><a href="">This is one</a></li>
+                                    <li><a href="">This is one</a></li>
+                                    <li><a href="">This is one</a></li>
+                                </ul>               
+                            </div>
+                         </div>
+                    </div>
+                    <div class="accordion-group">
+                        <div class="accordion-heading">
+                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#collapseFour">
+                                <i class="icon-list-alt"></i> Forms
+                            </a>
+                        </div>
+                        <div id="collapseFour" class="accordion-body collapse" style="height: 0px; ">
+                            <div class="accordion-inner">
+                                <ul>
+                                    <li><a href="">This is one</a></li>
+                                    <li><a href="">This is one</a></li>
+                                    <li><a href="">This is one</a></li>
+                                </ul>                
+                            </div>
+                         </div>
+                    </div>
+                    <div class="accordion-group">
+                        <div class="accordion-heading">
+                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#collapseFive">
+                                <i class="icon-cog"></i> Plugins
+                            </a>
+                        </div>
+                        <div id="collapseFive" class="accordion-body collapse" style="height: 0px; ">
+                            <div class="accordion-inner">
+                                <ul>
+                                    <li><a href="">This is one</a></li>
+                                    <li><a href="">This is one</a></li>
+                                    <li><a href="">This is one</a></li>
+                                </ul>                
+                            </div>
+                         </div>
+                    </div>
+                    <div class="accordion-group">
+                        <div class="accordion-heading">
+                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#collapseSix">
+                                <i class="icon-file"></i> Templates
+                            </a>
+                        </div>
+                        <div id="collapseSix" class="accordion-body collapse" style="height: 0px; ">
+                            <div class="accordion-inner">
+                                <ul>
+                                    <li><a href="">This is one</a></li>
+                                    <li><a href="">This is one</a></li>
+                                    <li><a href="">This is one</a></li>
+                                </ul>                
+                            </div>
+                         </div>
+                    </div>
+                </div>
+      </div>     
+      <div class="main-content">
+        <div class="journal-image">
+          <img src="img/bussines.jpg">
+          <div class="caption">
+            <div class="date">
+              <span class="year">2014</span><br>
+              UPDATED 5.25
+            </div>
+            Business
+            <span class="normal">JOURNAL</span>
+          </div>
+        </div>
+        <div class="journal-carrousel ">
+          <div class="col-xs-4">
+            <div class="wrap-journal">
+              <img src="img/bussines_s1.jpg">
+              <div class="caption">LONG LISTINGS</div>              
+            </div>
+          </div>
+          <div class="col-xs-4">
+            <div class="wrap-journal">
+              <img src="img/bussines_s2.jpg">
+              <div class="caption">LONG LISTINGS</div>              
+            </div>
+          </div>
+          <div class="col-xs-4">
+            <div class="wrap-journal">
+              <img src="img/bussines_s3.jpg">
+              <div class="caption">LONG LISTINGS</div>              
+            </div>
+          </div>                    
+
+        </div>      
+        <div class="news-home">
+          <h1>PRCA Business Journal <span>News</span></h1>
+          The business journal pages serve as the official record of the business for the Professional Rodeo Cowboys Association. Important Notices, Announcments, and Contact numbers are featured in this section
+            <div class="panel-group" id="accordion">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" 
+                      href="#collapse-1">
+                      ABBREVIATIONS KEY 
+                      <i class="glyphicon glyphicon-chevron-down"></i>
+                    </a>
+                  </h4>
+                </div>
+                <div id="collapse-1" class="panel-collapse collapse in">
+                  <div class="panel-body">
+                    <div class="table-responsive">
+                      <table class="table">
+                        <tbody>
+                            <tr>
+                              <td class="weight_bold">BB</td>
+                              <td>Bareback</td>
+                              <td class="weight_bold">TD</td> 
+                              <td>tie-down Roping</td>
+                              <td class="weight_bold">HL</td>
+                              <td>Heeler</td>
+                            </tr>
+                            <tr>
+                              <td class="weight_bold">SB</td>
+                              <td>Saddle Bronc</td>
+                              <td class="weight_bold">TR</td> 
+                              <td>Team Roping</td>
+                              <td class="weight_bold">SR</td>
+                              <td>Steer Roping</td>
+                            </tr>
+                            <tr>
+                              <td class="weight_bold">BR</td>
+                              <td>Bull Fiding</td>
+                              <td class="weight_bold">HD</td> 
+                              <td>Header</td>
+                              <td class="weight_bold">GB</td>
+                              <td>Girl Barrel Racing</td>
+                            </tr>
+                            <tr>
+                              <td class="weight_bold">SW</td>
+                              <td>Steer Wrestling</td>
+                              <td class="weight_bold"></td> 
+                              <td></td>
+                              <td class="weight_bold"></td>
+                              <td></td>
+                            </tr>
+                            <tr>
+                              <td class="weight_bold">NPP</td>
+                              <td>No priority positions </td>
+                              <td class="weight_bold">MTD</td> 
+                              <td>Mountaing Daylinght Time</td>
+                              <td class="weight_bold">HL</td>
+                              <td>Heeler</td>
+                            </tr>
+                            <tr>
+                              <td class="weight_bold">EOO</td>
+                              <td>Entry Office Opens</td>
+                              <td class="weight_bold">EC</td> 
+                              <td>Entries Close</td>
+                              <td class="weight_bold">EF</td>
+                              <td>Entry Fees</td>
+                            </tr>
+                            <tr>
+                              <td class="weight_bold">MT</td>
+                              <td>Mountain Time</td>
+                              <td class="weight_bold"></td> 
+                              <td></td>
+                              <td class="weight_bold">W/F</td>
+                              <td>With Finals</td>
+                            </tr>
+                          </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" 
+                      href="#collapse-2">
+                       2014 PRCA Donita Barnes Contract Personnel Lifetime Achievement award
+                      <i class="glyphicon glyphicon-chevron-down"></i>
+                    </a>
+                  </h4>
+                </div>
+                <div id="collapse-2" class="panel-collapse collapse">
+                  <div class="panel-body">
+                    The five finalists for the PRCA Donita Barnes Contract  Personnel Lifetime ...
+                  </div>
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" 
+                      href="#collapse-3">
+                      New photo website for PRCA members 
+                      <i class="glyphicon glyphicon-chevron-down"></i>
+                    </a>
+                  </h4>
+                </div>
+                <div id="collapse-3" class="panel-collapse collapse">
+                  <div class="panel-body">
+                    The PRCA has launched a new website within Prorodeo.com for members and fans to purchase rodeo images from PRCA Photographers...
+                  </div>
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" 
+                      href="#collapse-4">
+                     New photo website for PRCA members  
+                      <i class="glyphicon glyphicon-chevron-down"></i>
+                    </a>
+                  </h4>
+                </div>
+                <div id="collapse-4" class="panel-collapse collapse">
+                  <div class="panel-body">
+                    <p>The PRCA has launched a new website within Prorodeo.com for members and fans to purchase rodeo images from PRCA Photographers.</p>
+                    <img src="img/news-logo.png" class="alignleft">
+                    <p>If you missed buying your photo at a recent PRCA rodeo, you can search the website by event and purchase the photos directly from the site. </p>
+
+                    <p>This new tool allows PRCA Photographers to reach a greater audience and now provides a place for fans to search for their favorite contestant and purchase one or multiple images. As the site is still in the beginning stages, please keep in mind that the list of rodeos posted is growing, so check back often.</p>
+
+                    <p>To learn more, visit <a href="">prorodeophotos.com</a></p>  
+                  </div>
+                </div>
+              </div>  
+            </div>        
+        </div>
+
+        <?php include("footer.php");?>
+      </div>
+     
+    </div>
+
+  </div>  
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
---
+++ footer.php
@@ -0,0 +1,9 @@
+        <footer id="footer">
+          <div class="copy">
+          Copyright© 2014 Professional Rodeo  Cowboys Asociation.<br>
+          All Rights Reserved.
+          </div>
+          <ul class="footer-nav">
+            <li><a href="">CONTACT</a></li><li><a href="">BACK TO PRCA</a></li>
+          </ul>
+        </footer>
